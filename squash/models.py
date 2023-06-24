f7d1b0e0b2af0dd94a625690fe515f804213e983
Tonywilson1211 <>
2023-06-24 14:47:23 +0000

    add Comment model

--- squash/models.py
+++ squash/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment")
+    email = models.EmailField()
+    updated_on = models.DateTimeField(auto_now=True)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name="comment_likes", blank=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
