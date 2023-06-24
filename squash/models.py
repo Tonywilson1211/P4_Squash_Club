dfdb5776e709dae28b6d67ce09b26bd3d2dde655
Tonywilson1211 <>
2023-06-24 15:10:34 +0000

    add site_id to settings.py

--- squash/models.py
+++ squash/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+STATUS = ((0, "Draft"), (1, "Published"))
+
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment")
